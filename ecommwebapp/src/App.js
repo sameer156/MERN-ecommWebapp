a29215d06377f1adb12755fe61874eccf746a261
Sameer Vashist <vashistsameer4@gmail.com>
2022-05-09 11:55:47 +0530

    Creating_react-app

---
+++ ecommwebapp/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ecommwebapp</a>
+      </header>
+      <main>List of products</main>
+    </div>
+  );
+}
+
+export default App;
